473c96affaa3667da79c8f40ad7949dbc0784365
pruett <sukesake@gmail.com>
2014-02-24 21:09:20 -0800

    cows can walk

---
+++ Assets/circleWalk.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class circleWalk : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	/* the object to orbit */
+	public Transform target;
+	
+	/* speed of orbit (in degrees/second) */
+	public float speed;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		if (target != null) {
+			transform.RotateAround(target.position, Vector3.up, -speed * Time.deltaTime);
+		}
+	}
+}
