1c0c048bd65fa4b9816c595272f0e724aa616521
Proehl <chrispytoast@gmail.com>
2014-02-25 23:29:30 -0800

    Changed Character Controller to feel better. Fixed diagonal acceleration problem, partially fixed jumping on walls, changed move speed and added slight mid air direction change

--- Assets/Player/Scripts/PlayerController.cs
+++ Assets/Player/Scripts/PlayerController.cs
@@ -6,28 +6,40 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 	
-	public float speed = 10.0f;
+	public float ForwardSpeed = 12.0f;
+	public float SidewaysSpeed = 8.0f;
+	public float MaxSpeed = 12.0f;
 	public float gravity = 10.0f;
 	public float maxVelocityChange = 10.0f;
+	public float maxAirVelocityChange = 0.5f;
 	public bool canJump = true;
-	public float jumpHeight = 2.0f;
+	public float jumpHeight = 4.0f;
 	private bool grounded = false;
 	public float sensitivityX = 15F;
 	
 	
 	
-	void Awake () {
+	void Awake () 
+	{
 		rigidbody.freezeRotation = true;
 		rigidbody.useGravity = false;
 	}
 	
-	void FixedUpdate () {
-		if (grounded) {
-			// Calculate how fast we should be moving
-			Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-			targetVelocity = transform.TransformDirection(targetVelocity);
-			targetVelocity *= speed;
-			
+	void FixedUpdate () 
+	{
+		// Calculate how fast we should be moving
+		Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		targetVelocity.x *= SidewaysSpeed;
+		targetVelocity.z *= ForwardSpeed;
+		targetVelocity = transform.TransformDirection(targetVelocity);
+		if(targetVelocity.magnitude > MaxSpeed)
+		{
+			targetVelocity.Normalize();
+			targetVelocity *= MaxSpeed;
+		}
+		
+		if (grounded) 
+		{
 			// Apply a force that attempts to reach our target velocity
 			Vector3 velocity = rigidbody.velocity;
 			Vector3 velocityChange = (targetVelocity - velocity);
@@ -37,9 +49,20 @@ public class PlayerController : MonoBehaviour {
 			rigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
 			
 			// Jump
-			if (canJump && Input.GetButton("Jump")) {
+			if (canJump && Input.GetButton("Jump")) 
+			{
 				rigidbody.velocity = new Vector3(velocity.x, CalculateJumpVerticalSpeed(), velocity.z);
 			}
+		}
+		else
+		{
+			// Apply a force that attempts to reach our target velocity
+			Vector3 velocity = rigidbody.velocity;
+			Vector3 velocityChange = (targetVelocity - velocity);
+			velocityChange.x = Mathf.Clamp(velocityChange.x, -maxAirVelocityChange, maxAirVelocityChange);
+			velocityChange.z = Mathf.Clamp(velocityChange.z, -maxAirVelocityChange, maxAirVelocityChange);
+			velocityChange.y = 0;
+			rigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
 		}
 		
 		// We apply gravity manually for more tuning control
@@ -50,11 +73,19 @@ public class PlayerController : MonoBehaviour {
 		transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
 	}
 	
-	void OnCollisionStay () {
-		grounded = true;    
+	void OnCollisionStay (Collision collisionInfo) 
+	{
+		foreach(ContactPoint c in collisionInfo.contacts)
+		{
+			if(Mathf.Abs(c.normal.y) > Mathf.Abs(c.normal.x))
+			{
+				grounded = true;
+			}
+		}		    
 	}
 	
-	float CalculateJumpVerticalSpeed () {
+	float CalculateJumpVerticalSpeed () 
+	{
 		// From the jump height and gravity we deduce the upwards speed 
 		// for the character to reach at the apex.
 		return Mathf.Sqrt(2 * jumpHeight * gravity);
