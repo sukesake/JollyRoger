0f986221f9c0cfdc6d9a4ca3bfea83f89034d499
pruett <sukesake@gmail.com>
2014-05-26 12:41:22 -0700

    voxel project

---
+++ Voxel/Tutorial4/Block.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial4
+{
+    class Block
+    {
+
+        public bool IsActive { get; set; }
+
+        private bool m_active;
+
+        private BlockType m_blockType;
+
+    }
+}
---
+++ Voxel/Tutorial4/BlockType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial4
+{
+    enum BlockType
+    {
+        Default,
+        Grass,
+        Dirt,
+        Water,
+        Stone,
+        Wood,
+        Sand,
+    };
+}
