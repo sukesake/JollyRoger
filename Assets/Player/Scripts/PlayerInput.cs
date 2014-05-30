41255a628e844e558f891c5b487cb207886795f4
pruett <sukesake@gmail.com>
2014-05-29 18:44:30 -0700

    remove the unity stuff and clean stuff up

--- Assets/Player/Scripts/PlayerInput.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public partial class PlayerController : MonoBehaviour
-{
-   // private static Vector3 GetDirectionalInput()
-//    {
-//     //   return new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-//    }
- }
