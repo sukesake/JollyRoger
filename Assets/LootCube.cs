271064379d4160bb24ee140e4fdb6594263eb181
pruett <sukesake@gmail.com>
2014-11-19 00:00:21 -0800

    make loot lootable

--- Assets/LootCube.cs
+++ Assets/LootCube.cs
@@ -8,15 +8,25 @@ public class LootCube : MonoBehaviour
 
     public int MaxHorizontalSpread = 1;
     public int MaxVerticalHeight = 10;
-	// Use this for initialization
+    public long ItemNumber { get; set; }
+    public long Quantity { get; set; }
+
+    // Use this for initialization
 	void Start ()
 	{
 	    var rnd = new Random();
         transform.rigidbody.AddExplosionForce(3, transform.position, 1);
+	    ItemNumber = rnd.Next(1, 10);
+	    Quantity = rnd.Next(1, 3);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    void Destroy()
+    {
+        Debug.Log("DEEEESTORY");
+    }
 }
